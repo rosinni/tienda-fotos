590e524e1437b697f4ef052fae49b5656e84c2ee
Rosinni Rodriguez <ros.rm169@gmail.com>
2020-07-10 00:48:38 -0300

    Updates

---
+++ precache-manifest.7887ac2c6108741608a7bd9ed581feee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715d04324fed398ec13792b8c443d677",
+    "url": "/tienda-fotos/index.html"
+  },
+  {
+    "revision": "fc36ed6ef03d619041aa",
+    "url": "/tienda-fotos/static/css/main.e4be8b79.chunk.css"
+  },
+  {
+    "revision": "f689a01c77a3b5993517",
+    "url": "/tienda-fotos/static/js/2.991255ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tienda-fotos/static/js/2.991255ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc36ed6ef03d619041aa",
+    "url": "/tienda-fotos/static/js/main.415ace9f.chunk.js"
+  },
+  {
+    "revision": "1ca88121679b97bde36b",
+    "url": "/tienda-fotos/static/js/runtime-main.6e2c2953.js"
+  }
+]);
